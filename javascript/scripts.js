b2fde5e2c4c54daf4eaa285fe20696ceb0ad9f53
Jaime González <arcarius11@gmail.com>
2019-03-16 21:36:37 -0300

    Terminando la webstore

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,9 +1,11 @@
-$(".formulario").focus (function(){
+$(".thumbnail img").addClass("imagenes")
+
+/*$(".formulario").focus (function(){
     var seleccion = val("")
     if ($("option :selected").val("Si")){
         $(".formulario").html("<div>ASDADADA</div>")
     } else {
         $("body").css("background-color", "yellow")
     }
-})
-/*$('select option:selected("Si")').append("")*/
+})*/
+
